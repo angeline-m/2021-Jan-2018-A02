ddc6a3631260bc7d9bec5044e8a2202ecdfa54ae
angeline-m <shelite17@gmail.com>
2021-01-15 16:09:41 -0700

    create web app page for filter search, setup title and search area, create code behind to load DDL

---
+++ ChinookSolution/WebApp/SamplePages/SearchByDDL.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class SearchByDDL : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //this is first time
+                LoadArtistList();
+
+            }
+        }
+
+        protected void LoadArtistList()
+        {
+            ArtistController sysmgr = new ArtistController();
+            List<SelectionList> info = sysmgr.Artists_DDLList();
+
+            //let's assume the data collection needs to be sorted
+            info.Sort((x,y) => x.DisplayField.CompareTo(y.DisplayField));
+
+            //setup the ddl
+            ArtistList.DataSource = info;
+
+            //OLD WAY
+            //ArtistList.DataTextField = "Displayfield";
+
+            //NEW WAY
+            ArtistList.DataTextField = nameof(SelectionList.DisplayField);
+            ArtistList.DataValueField = nameof(SelectionList.ValueField);
+            ArtistList.DataBind();
+
+            //prompt line -- a more "complete and proper" way of inserting a prompt
+            ArtistList.Items.Insert(0, new ListItem("Select...", "0"));
+        }
+
+        protected void SearchAlbums_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
